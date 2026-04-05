468c631608800532e4a54ca2d738862b2cb2259b
dmoneybaron-dev <dmoneybaron@gmail.com>
2026-04-05 01:33:01 +0100

    Add files via upload
    Initial project upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const products = [
+  { name: "Laptop", price: 900, category: "Electronics" },
+  { name: "Shoes", price: 120, category: "Clothing" },
+  { name: "Watch", price: 250, category: "Gadgets" },
+  { name: "Bag", price: 80, category: "Accessories" }
+];
+
+
+
+const container = document.getElementById("products");
+products.forEach(product=>container.innerHTML+=produ);
